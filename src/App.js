d0a08fe23bf0b59298724d8f4c00841720a25683
Stéphane Morel <110528810+Steph4477@users.noreply.github.com>
2023-04-13 12:20:09 +0200

    delete route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Nav from './components/Nav';
-import Home from './pages/home';
+import Home from './pages/Home';
 import './styles/Index.scss';
 
 
@@ -12,11 +12,7 @@ export default function App() {
   return (
     <div className="App">
       <Nav />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
+      <Home />  
+      </div>
   );
 }
