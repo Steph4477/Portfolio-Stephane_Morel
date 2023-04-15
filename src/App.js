ebce62c6150b4560296e19838930c6912cf29281
Stéphane Morel <110528810+Steph4477@users.noreply.github.com>
2023-04-15 11:21:36 +0200

    update card-project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Nav from './components/Nav';
 import Home from './pages/home';
 import './styles/Index.scss';
@@ -12,11 +12,8 @@ export default function App() {
   return (
     <div className="App">
       <Nav />
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <Home />
+   
     </div>
-  );
+  )
 }
